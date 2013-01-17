53a60620e4e3ed24a38663b1ef65a210924994eb
galaran <galaran.h@gmail.com>
2013-01-17 12:16:39 +0400

    Rewrite SummonWolfPack

--- SkillSummonWolfPack/src/mccity/heroes/skills/summonwolfpack/SkillSummonWolfPack.java
+++ SkillSummonWolfPack/src/mccity/heroes/skills/summonwolfpack/SkillSummonWolfPack.java
@@ -13,39 +13,79 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.*;
-
-import java.util.List;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Wolf;
 
 public class SkillSummonWolfPack extends ActiveSkill {
 
     public SkillSummonWolfPack(Heroes plugin) {
         super(plugin, "SummonWolfPack");
-        setDescription("100% chance to spawn 1 wolf, $1% for 2, and $2% for 3");
+        setDescription("100% chance to spawn 1 wolf, $1% for 2, and $2% for 3.");
         setUsage("/skill wolfpack");
         setArgumentRange(0, 0);
         setIdentifiers("skill wolfpack", "skill summonwolfpack");
         setTypes(SkillType.SUMMON, SkillType.SILENCABLE, SkillType.EARTH);
     }
 
-    public String getDescription(Hero hero) {
-        int chance2x = Math.min((int) (100.0 * getChance(hero, 2)), 100);
-        int chance3x = Math.min((int) (100.0 * getChance(hero, 3)), 100);
-        return getDescription().replace("$1", String.valueOf(chance2x)).replace("$2", String.valueOf(chance3x));
-    }
-
     public ConfigurationSection getDefaultConfig() {
         ConfigurationSection defaultConfig = super.getDefaultConfig();
+        defaultConfig.set(Setting.MAX_DISTANCE.node(), 5.0);
+        defaultConfig.set(Setting.MAX_DISTANCE_INCREASE.node(), 0.1);
         defaultConfig.set(Setting.REAGENT.node(), 352);
-        defaultConfig.set(Setting.REAGENT_COST.node(), 2);
+        defaultConfig.set(Setting.REAGENT_COST.node(), 0);
         defaultConfig.set("base-chance-2x", 0.75);
-        defaultConfig.set("base-chance-3x", 0.5);
-        defaultConfig.set(Setting.MAX_DISTANCE.node(), 20);
         defaultConfig.set("chance-2x-per-level", 0.0);
+        defaultConfig.set("base-chance-3x", 0.5);
         defaultConfig.set("chance-3x-per-level", 0.0);
-        defaultConfig.set(Setting.COOLDOWN.node(), 120000);
-        defaultConfig.set(Setting.HEALTH.node(), 20);
         return defaultConfig;
+    }
+
+    public String getDescription(Hero hero) {
+        StringBuilder descr = new StringBuilder(getDescription()
+                .replace("$1", Util.stringDouble(getChance(hero, 2) * 100))
+                .replace("$2", Util.stringDouble(getChance(hero, 3) * 100))
+        );
+
+        int cd = getCooldown(hero);
+        if (cd > 0) {
+            descr.append(" CD:");
+            descr.append(Util.stringDouble(cd / 1000.0));
+            descr.append("s");
+        }
+
+        int mana = getMana(hero);
+        if (mana > 0) {
+            descr.append(" M:");
+            descr.append(mana);
+        }
+
+        descr.append(" MaxDist:");
+        descr.append(String.valueOf(getMaxDistance(hero)));
+        
+        return descr.toString();
+    }
+
+    public int getMaxDistance(Hero hero) {
+        return (int) (SkillConfigManager.getUseSetting(hero, this, Setting.MAX_DISTANCE, 5.0, false) +
+                SkillConfigManager.getUseSetting(hero, this, Setting.MAX_DISTANCE_INCREASE, 0.1, false) * hero.getSkillLevel(this));
+    }
+
+    public int getCooldown(Hero hero) {
+        return Math.max(0, SkillConfigManager.getUseSetting(hero, this, Setting.COOLDOWN, 0, true) -
+                SkillConfigManager.getUseSetting(hero, this, Setting.COOLDOWN_REDUCE, 0, false) * hero.getSkillLevel(this));
+    }
+
+    public int getMana(Hero hero) {
+        return (int) Math.max(0.0, SkillConfigManager.getUseSetting(hero, this, Setting.MANA, 0.0, true) -
+                SkillConfigManager.getUseSetting(hero, this, Setting.MANA_REDUCE, 0.0, false) * hero.getSkillLevel(this));
+    }
+
+    public double getChance(Hero hero, int count) {
+        String countString = String.valueOf(count);
+        double chance = SkillConfigManager.getUseSetting(hero, this, "base-chance-$x".replace("$", countString), count == 2 ? 0.75 : 0.5, false) +
+                SkillConfigManager.getUseSetting(hero, this, "chance-$x-per-level".replace("$", countString), 0.0, false) * hero.getSkillLevel(this);
+        return Math.min(chance, 1.0);
     }
 
     public SkillResult use(Hero hero, String[] args) {
@@ -53,20 +93,17 @@ public class SkillSummonWolfPack extends ActiveSkill {
 
         double chance2x = getChance(hero, 2);
         double chance3x = getChance(hero, 3);
-        int maxDistance = SkillConfigManager.getUseSetting(hero, this, Setting.MAX_DISTANCE, 20, false);
+        int maxDistance = getMaxDistance(hero);
         Block targetBlock = player.getTargetBlock(null, maxDistance).getRelative(BlockFace.UP);
-        if (targetBlock == null) {
+        if (targetBlock == null) { // y >= 256
             return SkillResult.CANCELLED;
         }
 
         // remove all player's wolves in 100m
-        List<Entity> nearEntities = player.getNearbyEntities(100, 100, 100);
-        for (Entity nearEntity : nearEntities) {
-            if (nearEntity instanceof Wolf) {
-                Wolf nearWolf = (Wolf) nearEntity;
-                if (nearWolf.isTamed() && nearWolf.getOwner().getName().equals(player.getName())) {
-                    nearWolf.remove();
-                }
+        for (Wolf wolf : player.getWorld().getEntitiesByClass(Wolf.class)) {
+            if (wolf.isTamed() && wolf.getOwner().getName().equals(player.getName())
+                    && wolf.getLocation().distanceSquared(player.getLocation()) <= 10000) {
+                wolf.remove();
             }
         }
 
@@ -75,15 +112,12 @@ public class SkillSummonWolfPack extends ActiveSkill {
         if (roll < chance2x) wolfAmount++;
         if (roll < chance3x) wolfAmount++;
 
-        int wolfHp = SkillConfigManager.getUseSetting(hero, this, Setting.HEALTH, 20, false);
-        wolfHp = Math.max(1, Math.min(wolfHp, 20));
-
         for (int i = 0; i < wolfAmount; i++) {
             // add some xz dispersion
             Location wolfSpawnLoc = targetBlock.getLocation().clone();
             wolfSpawnLoc.add(Util.nextInt(5) - 2, 0, Util.nextInt(5) - 2);
 
-            // correct spawn location to not spawn wolf inside a solid block
+            // correct spawn location - not spawn inside a block
             int upShifting = 0;
             Block testBlock = wolfSpawnLoc.getBlock();
             while (testBlock.getType() != Material.AIR ||
@@ -102,15 +136,9 @@ public class SkillSummonWolfPack extends ActiveSkill {
 
             Wolf wolf = (Wolf) player.getWorld().spawnEntity(wolfSpawnLoc, EntityType.WOLF);
             wolf.setOwner(player);
-            wolf.setHealth(wolfHp);
         }
+        
         broadcastExecuteText(hero);
         return SkillResult.NORMAL;
-    }
-
-    private double getChance(Hero hero, int count) {
-        String countString = String.valueOf(count);
-        return SkillConfigManager.getUseSetting(hero, this, "base-chance-$x".replace("$", countString), count == 2 ? 0.75 : 0.5, false) +
-                SkillConfigManager.getUseSetting(hero, this, "chance-$x-per-level".replace("$", countString), 0.0, false) * hero.getSkillLevel(this);
     }
 }
